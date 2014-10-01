8b5d24898b3311081021ecb38da18a6e3f447040
esoares <esoares@dcc-lab3-01>
2014-10-01 11:42:19 -0300

    mudança no layout do perfil do aluno, alterações ainda não finalizadas.

--- src/br/ufjf/egresso/controller/FbAuthController.java
+++ src/br/ufjf/egresso/controller/FbAuthController.java
@@ -7,12 +7,14 @@ import org.zkoss.zk.ui.Page;
 import org.zkoss.zk.ui.Sessions;
 import org.zkoss.zk.ui.util.Initiator;
 
+import br.ufjf.egresso.utils.ConfHandler;
+
 public class FbAuthController implements Initiator {
 
 	@Override
 	public void doInit(Page page, Map<String, Object> args) throws Exception {
 		if (Sessions.getCurrent()
 				.getAttribute("aluno") == null)
-			Executions.sendRedirect("https://apps.facebook.com/ufjf-dcc-egresso/");
+			Executions.sendRedirect(ConfHandler.getConf("FB.CANVASPAGE"));
 	}
 }
